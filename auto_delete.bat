733af9482298e73558e4bb53e54ca1667e1d9225
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-04-05 10:01:54 -0300

    Delete auto_delete.bat

--- auto_delete.bat
+++
@@ -1,7 +0,0 @@
-@echo Excluindo Arquivos
-pause
-set folder="G:\Documentos\Arquivos"
-set days=1
-forfiles /p %folder% /s /m *.* /d -%days% /c "cmd /c echo Excluindo @file & del /q @path"
-@echo Done
-pause
