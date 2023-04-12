206b85fede1997f2da05dc7d8ca5945bf4ee55e3
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-04-12 17:03:35 -0300

    Update Dados Impostos PCMOV.sql
    Continuando a consulta de impostos.

--- Consultas 04-11-2023/Dados Impostos PCMOV.sql
+++ Consultas 04-11-2023/Dados Impostos PCMOV.sql
@@ -1,5 +1,13 @@
-SELECT DISTINCT(codprod),ST,SITTRIBUT,baseicms,baseicst,ALIQICST,codplpag,percipi,pericm,percst,vlbaseipi,vlipi,
-PERICMSANTECIPADO,numlanccp,codfiscal,codst,perpis,VLCREDPIS,VLCREDICMS,PERICMFRETE,CODFUNCREQ,VLDESCICMISENCAO,CUSTOREALSEMST,
-IVA,ALIQICMS1,ALIQICMS2,CUSTOFINEST
-FROM PCMOV
+SELECT DISTINCT(a.codprod)|| ' - ' || b.descricao produto, 
+        b.codfornec || ' - ' ||c.fornecedor fornecedor, 
+        b.codsec || ' - ' || d.descricao seção
+        ,a.ST,a.SITTRIBUT,a.baseicms,a.baseicst,a.codplpag,a.percipi,a.pericm,a.percst,a.vlbaseipi,a.vlipi,
+        a.codfiscal,a.codst,a.perpis,a.VLCREDPIS,a.VLCREDICMS,a.CUSTOREALSEMST,a.IVA,a.ALIQICMS1,a.ALIQICMS2,
+        a.CUSTOFINEST
+FROM PCMOV a
+    JOIN PCprodut b on a.codprod = b.codprod
+    JOIN pcfornec c on b.codfornec = c.codfornec
+    JOIN pcsecao d on b.codsec = d.codsec
 WHERE DTMOV > '01-jan-2023'
+
+--select * from pcsecao
