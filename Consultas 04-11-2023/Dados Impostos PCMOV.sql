e3852b0ae0b80d2ef78a8c602e14c1b729341c17
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-04-13 11:15:30 -0300

    Update Dados Impostos PCMOV.sql
    Continuando a construção da consulta sobre sobre situação dos impostos nos produtos da empresa.

--- Consultas 04-11-2023/Dados Impostos PCMOV.sql
+++ Consultas 04-11-2023/Dados Impostos PCMOV.sql
@@ -1,13 +1,35 @@
 SELECT DISTINCT(a.codprod)|| ' - ' || b.descricao produto, 
-        b.codfornec || ' - ' ||c.fornecedor fornecedor, 
-        b.codsec || ' - ' || d.descricao seção
-        ,a.ST,a.SITTRIBUT,a.baseicms,a.baseicst,a.codplpag,a.percipi,a.pericm,a.percst,a.vlbaseipi,a.vlipi,
-        a.codfiscal,a.codst,a.perpis,a.VLCREDPIS,a.VLCREDICMS,a.CUSTOREALSEMST,a.IVA,a.ALIQICMS1,a.ALIQICMS2,
-        a.CUSTOFINEST
+        b.codfornec || ' - ' ||c.fornecedor fornecedor,
+        
+        a.  SITTRIBUT   ,
+        a.  IVA ,
+        a.  BASEICMS    ,
+        a.  PERCICM ,
+        a.  ALIQICMS1   ,
+        a.  ALIQICMS2   ,
+        a.  BASEICMSBCR ,
+        a.  VLICMSBCR   ,
+        a.  CODICMTAB   ,
+        a.  QTSALDOEST  ,
+        a.  CODST   ,
+        a.  CUSTOREALSEMSTANT   ,
+        a.  CUSTOREALSEMST  ,
+        a.  CUSTOFINEST ,
+        a.  PERCBASEREDST   ,
+        a.  STBCR   ,
+        a.  PERPIS  ,
+        a.  VLCREDPIS   ,
+        a.  VLPIS   ,
+        a.  PERCBASERED ,
+        a.  BASEBCR ,
+        a.  PERCOFINS   ,
+        a.  VLCREDCOFINS    ,
+        a.  PVENDA1
+
 FROM PCMOV a
     JOIN PCprodut b on a.codprod = b.codprod
     JOIN pcfornec c on b.codfornec = c.codfornec
     JOIN pcsecao d on b.codsec = d.codsec
 WHERE DTMOV > '01-jan-2023'
 
---select * from pcsecao
+--select * from pcmov WHERE DTMOV > '01-jan-2023'
