c3e5b6538d029986bacf368d99d097473448d86d
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-04-14 15:02:40 -0300

    Pedido vs Estoque
    Após verificar, foi constado uma divergência de valores. Essa mudança na consulta foi feita para corrigir essa diferença.

--- Consultas 06-04-2023/Pedido vs Estoque.sql
+++ Consultas 06-04-2023/Pedido vs Estoque.sql
@@ -1,14 +1,13 @@
 SELECT Distinct a.CODPROD, c.descricao, 
-       SUM(a.QT) qtPedidos, (b.qtest - b.qtreserv) qtEstoque, 
+       SUM(a.QT) qt_Pedidos, (b.qtest - b.qtreserv) qt_Estoque, 
        SUM(a.QT) - (b.qtest - b.qtreserv) corte_previsto
 from pcpedi a
-JOIN PCest b on a.codprod = b.codprod
-join pcprodut c on a.codprod = c.codprod
+    JOIN PCest b on a.codprod = b.codprod
+    join pcprodut c on a.codprod = c.codprod
 WHERE a.DATA > '02-abr-2023'
-AND a.CODUSUR Not In (140,141,142,143,153,164,157,2,10)
-AND a.posicao != 'F'
-AND (a.QT - b.qtest) > 0
-and b.codfilial = 3
---AND c.descricao like '%YOPRO%'
+    AND a.CODUSUR Not In (140,141,142,143,153,164,157,2,10)
+    AND a.posicao != 'F'
+    AND (a.QT - b.qtest) > 0
+    and b.codfilial = 3
+    --AND c.descricao like '%YOPRO%'
 group by a.codprod,c.descricao,b.qtest,b.qtreserv
---select * from pcest where codprod = 18034
