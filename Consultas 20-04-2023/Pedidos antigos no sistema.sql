cdb12521707314ce714a212075a60f45ac1cb326
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-04-25 09:01:12 -0300

    Update Pedidos antigos no sistema.sql
    Ajustes na consulta

--- Consultas 20-04-2023/Pedidos antigos no sistema.sql
+++ Consultas 20-04-2023/Pedidos antigos no sistema.sql
@@ -1,6 +1,15 @@
-SELECT DISTINCT a.NUMPED,a.codusur,a.posicao,a.data
+SELECT DISTINCT(a.NUMPED),a.codusur,a.posicao,a.data || ' às ' || b.hora||'H' "Envio",
+                b.obs "MOTIVO ANALISADO",c.motivo "MOTIVO SISTEMA"
+                
 FROM pcpedi a
-WHERE (SYSDATE - a.DATA > 6) and  (SYSDATE - a.DATA < 30)
-AND a.posicao != 'F' AND a.posicao != 'C'
-AND a.codusur in (140,141,142,145,148,150,151,152,153,155,156,
-                    157,158,164,167,168,169,170,172,174)
+    JOIN pcpedc b on a.numped = b.numped
+    JOIN PCBLOQUEIOSPEDIDO c on a.numped = c.numped
+    
+WHERE (SYSDATE - a.DATA > 3) and  (SYSDATE - a.DATA < 30) AND a.codusur in (140,141,142,145,148,150,151,152,155,156,157,158,167,168,169,170,172,174)
+--WHERE (SYSDATE - a.DATA > 5) and  (SYSDATE - a.DATA < 30) AND a.codusur in (153) --and b.hora <= 12
+--WHERE (SYSDATE - a.DATA > 5) and  (SYSDATE - a.DATA < 30) AND a.codusur in (164)
+
+    AND b.dtfat is null
+    
+
+
