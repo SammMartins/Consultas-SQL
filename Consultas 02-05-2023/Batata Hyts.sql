39210ab5d1a19a06f19354e380e2f18c53852a2a
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-11 08:36:03 -0300

    Update

--- Consultas 02-05-2023/Batata Hyts.sql
+++ Consultas 02-05-2023/Batata Hyts.sql
@@ -1,17 +1,26 @@
-SELECT a.codusur || SUBSTR(b.nome,7,50) "Vendedor",
-       (SUM(a.qt))/24 "Caixas Totais",
-       a.codprod || ' - ' || c.descricao Produto,
-       (CASE WHEN a.CODPROD in (18362,18179,18361,18180) THEN ( (SUM (a.qt) ) /24 ) * 2 ELSE 0 END) "CHIPS 40g R$",
-       (CASE WHEN a.CODPROD in (18182) THEN ( (SUM (a.qt) ) /24 ) * 3 ELSE 0 END) "PALHA 70g R$",
-       (CASE WHEN a.CODPROD in (18364,18365) THEN ( (SUM (a.qt) ) /24 ) * 2 ELSE 0 END) "PURUCA 70g R$"
-       
-       
-FROM PCMOV a 
-JOIN PCUSUARI b on a.codusur = b.codusur
-JOIN pcprodut c on a.codprod = c.codprod
+SELECT DISTINCT (a.codusur || SUBSTR(b.nome, 7, 50)) "Vendedor",
+    (SELECT SUM(d.qt) FROM pcpedi d
+        WHERE d.CODPROD IN (18362, 18179, 18361, 18180) AND d.data BETWEEN '1-mai-2023' AND SYSDATE
+        AND d.codusur = a.codusur) / 24 AS "Caixas de CHIPS 40g",
+    (SELECT SUM(d.qt) FROM pcpedi d
+        WHERE d.CODPROD IN (18182) AND d.data BETWEEN '1-mai-2023' AND SYSDATE
+        AND d.codusur = a.codusur) / 24 AS "Caixas de PALHA 70g",
+    (SELECT SUM(d.qt) FROM pcpedi d
+        WHERE d.CODPROD IN (18364, 18365) AND d.data BETWEEN '1-mai-2023' AND SYSDATE
+        AND d.codusur = a.codusur) / 24 AS "Caixas de PURUCA 70g",
+    (SELECT (SUM(CASE WHEN d.CODPROD IN (18362, 18179, 18361, 18180) THEN d.qt * 2 ELSE 0 END) +
+             SUM(CASE WHEN d.CODPROD IN (18182) THEN d.qt * 3 ELSE 0 END) +
+             SUM(CASE WHEN d.CODPROD IN (18364, 18365) THEN d.qt * 2 ELSE 0 END)) / 24 AS qt
+        FROM pcpedi d
+        WHERE d.codusur = a.codusur
+        and d.data BETWEEN '1-mai-2023' AND SYSDATE) AS "R$ Total"
+FROM pcpedi a
+JOIN PCUSUARI b ON a.codusur = b.codusur
+JOIN pcprodut c ON a.codprod = c.codprod
+WHERE a.CODPROD IN (18362, 18179, 18361, 18180, 18182, 18364, 18365)
+    AND a.data BETWEEN '1-mai-2023' AND SYSDATE
+    AND b.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+GROUP BY a.codprod, a.codusur, b.nome;
 
-WHERE a.CODPROD in (18362,18179,18361,18180,18182,18364,18365) 
-AND a.DTMOV BETWEEN '1-mai-2023' and SYSDATE
-AND b.codusur in (140,141,142,145,148,150,151,152,153,155,156,157,158,164,167,168,169,170,172,174)
-                    
-GROUP BY a.codusur,b.nome,a.codprod,c.descricao
+
+--select * from pcpedi
