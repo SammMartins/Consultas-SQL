687ad07849bd5e2e160aa38d357c446ccaadf476
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-11 11:01:21 -0300

    Create Batata Hyts v2.sql

---
+++ Consultas 11-05-2023/Batata Hyts v2.sql
@@ -0,0 +1,20 @@
+WITH caixas AS (
+    SELECT d.codusur,
+           SUM(CASE WHEN d.CODPROD IN (18362, 18179, 18361, 18180) THEN d.qt ELSE 0 END) AS caixas_chips,
+           SUM(CASE WHEN d.CODPROD IN (18182) THEN d.qt ELSE 0 END) AS caixas_palha,
+           SUM(CASE WHEN d.CODPROD IN (18364, 18365) THEN d.qt ELSE 0 END) AS caixas_puruca
+    FROM pcpedi d
+    WHERE d.data BETWEEN TO_DATE('1-mai-2023', 'DD-MON-YYYY') AND SYSDATE
+        AND d.posicao NOT LIKE 'C'
+    GROUP BY d.codusur
+)
+SELECT (u.codusur || ' - ' || u.nome) AS "Vendedor",
+       c.caixas_chips / 24 AS "Caixas de CHIPS 40g",
+       c.caixas_palha / 24 AS "Caixas de PALHA 70g",
+       c.caixas_puruca / 24 AS "Caixas de PURUCA 70g",
+       ((c.caixas_chips * 2) + (c.caixas_palha * 3) + (c.caixas_puruca * 2)) / 24 AS "R$ Total"
+FROM PCUSUARI u
+JOIN caixas c ON u.codusur = c.codusur
+WHERE u.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+ORDER BY "Vendedor";
+
