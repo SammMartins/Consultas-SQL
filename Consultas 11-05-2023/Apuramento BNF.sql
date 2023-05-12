4547b2daf51fc019a57a35c742f6204d8bfd54b6
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-12 11:23:25 -0300

    Update Apuramento BNF.sql

--- Consultas 11-05-2023/Apuramento BNF.sql
+++ Consultas 11-05-2023/Apuramento BNF.sql
@@ -4,8 +4,9 @@ WHERE EXISTS (
     SELECT *
     FROM PCPEDC C
     WHERE C.CONDVENDA IN (5, 11)
-        AND C.VLTOTAL > 400
+        AND C.VLTOTAL > 200
         AND C.CODCLI = a.CODCLI
+        AND a.Codcli != 11185
         AND C.DATA > SYSDATE - (366 * 2)
-    HAVING COUNT(*) > 3)
---AND a.Codcli != 11185
+    HAVING COUNT(*) > 5)
+
