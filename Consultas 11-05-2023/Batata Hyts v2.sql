8cac00af018b8899bbb1231f0ab77783fe1d419d
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-15 10:33:58 -0300

    Update Batata Hyts v2.sql

--- Consultas 11-05-2023/Batata Hyts v2.sql
+++ Consultas 11-05-2023/Batata Hyts v2.sql
@@ -1,4 +1,5 @@
 WITH caixas AS (
+    -- CTE (Common Table Expression) para calcular as quantidades de caixas por usuário
     SELECT d.codusur,
            SUM(CASE WHEN d.CODPROD IN (18362, 18179, 18361, 18180) THEN d.qt ELSE 0 END) AS caixas_chips,
            SUM(CASE WHEN d.CODPROD IN (18182) THEN d.qt ELSE 0 END) AS caixas_palha,
@@ -16,5 +17,15 @@ SELECT (u.codusur || ' - ' || u.nome) AS "Vendedor",
 FROM PCUSUARI u
 JOIN caixas c ON u.codusur = c.codusur
 WHERE u.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
-ORDER BY "Vendedor";
-
+----------------------------------------------------------------------------------------------------------------------------------------
+UNION ALL
+----------------------------------------------------------------------------------------------------------------------------------------
+SELECT 
+    'Desempenho Total da Equipe' AS "Vendedor",
+    SUM(c.caixas_chips / 24) AS "Caixas de CHIPS 40g",
+    SUM(c.caixas_palha / 24) AS "Caixas de PALHA 70g",
+    SUM(c.caixas_puruca / 24) AS "Caixas de PURUCA 70g",
+    SUM(((c.caixas_chips * 2) + (c.caixas_palha * 3) + (c.caixas_puruca * 2)) / 24) AS "R$ Total"
+    
+FROM caixas c
+    where c.codusur in (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
