e7df79137cfc80e02d7089a36d160d94f42c81bb
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-16 08:44:30 -0300

    Update Batata Hyts v2.sql

--- Consultas 11-05-2023/Batata Hyts v2.sql
+++ Consultas 11-05-2023/Batata Hyts v2.sql
@@ -5,20 +5,20 @@ WITH PC_Hyts AS (
            SUM(CASE WHEN d.CODPROD IN (18182) THEN d.qt ELSE 0 END) AS unidades_palha,
            SUM(CASE WHEN d.CODPROD IN (18364, 18365) THEN d.qt ELSE 0 END) AS unidades_puruca
     FROM pcpedi d
-    WHERE d.data BETWEEN TO_DATE('1-mai-2023', 'DD-MON-YYYY') AND SYSDATE
+    WHERE d.data BETWEEN '1-mai-2023' AND SYSDATE
         AND d.posicao NOT LIKE 'C'
-    GROUP BY d.codusur
-)
+    GROUP BY d.codusur)
+----------------------------------------------------------------------------------------------------------------------------------------
 SELECT (u.codusur || ' - ' || u.nome) AS "Vendedor",
        c.unidades_chips / 24 AS "Caixas de CHIPS 40g",
        c.unidades_palha / 24 AS "Caixas de PALHA 70g",
        c.unidades_puruca / 24 AS "Caixas de PURUCA 70g",
        ((c.unidades_chips * 2) + (c.unidades_palha * 3) + (c.unidades_puruca * 2)) / 24 AS "R$ Total"
 FROM PCUSUARI u
-JOIN PC_Hyts c ON u.codusur = c.codusur
+    JOIN PC_Hyts c ON u.codusur = c.codusur
 WHERE u.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
 ----------------------------------------------------------------------------------------------------------------------------------------
-UNION ALL
+UNION ALL --Utilizada para realizar a união de duas consultas, combinando os resultados em um único conjunto de resultados
 SELECT 
     'Desempenho Total da Equipe' AS "Vendedor",
     SUM(c.unidades_chips / 24) AS "Caixas de CHIPS 40g",
