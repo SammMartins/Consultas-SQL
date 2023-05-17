82e3e85b9bbae08401003d201a0e7441304e7c8c
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-17 14:52:08 -0300

    Update Batata Hyts v2.sql

--- Consultas 11-05-2023/Batata Hyts v2.sql
+++ Consultas 11-05-2023/Batata Hyts v2.sql
@@ -24,8 +24,9 @@ SELECT
     SUM(c.unidades_chips / 24) AS "Caixas de CHIPS 40g",
     SUM(c.unidades_palha / 24) AS "Caixas de PALHA 70g",
     SUM(c.unidades_puruca / 24) AS "Caixas de PURUCA 70g",
-    SUM(((c.unidades_chips * 2) + (c.unidades_palha * 3) + (c.unidades_puruca * 2)) / 24) AS "R$ Total"
-    
+    SUM(((c.unidades_chips * 2) + (c.unidades_palha * 3) + (c.unidades_puruca * 2)) / 24) AS "R$ Total"   
 FROM PC_Hyts c
     where c.codusur in (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+Order By "R$ Total"
+
 ----------------------------------------------------------------------------------------------------------------------------------------    
