96adc7fe16dba93772013aa17e4d0989d890f437
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-17 17:35:07 -0300

    Update Separação de Carga.sql

--- Consultas 17-05-2023/Separação de Carga.sql
+++ Consultas 17-05-2023/Separação de Carga.sql
@@ -1,11 +1,11 @@
 SELECT a.codprod,
        b.Descricao,
-       --b.embalagem,
-       a.qt || ' ' || b.unidade "Qtd.",
-       b.PESOBRUTO * a.QT PESOTOTAL,
-        b.qtunitcx
+       b.qtunitcx,
+       --(CASE WHEN (SUM(a.qt) / b.qtunitcx) >= 1 THEN (SUM(a.qt) / b.qtunitcx) || ' Caixas' Else SUM(a.qt) || ' Unidades' END) "Qtd. P/ Carga",
+       b.PESOBRUTO * SUM(a.qt) PESOTOTAL
  FROM PCPEDI a
     JOIN PCPRODUT b on a.codprod = b.codprod
     JOIN PCPEDC c on a.numped = c.numped
  Where c.numcar = 193969
+ GROUP BY a.codprod,b.Descricao, b.unidade,b.PESOBRUTO,a.QT,b.qtunitcx
 --Select * From PCPRODUT WHERE codprod = 17462
