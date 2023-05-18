79dfb9079e234860b913773edb4856383dac7350
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-18 09:55:23 -0300

    Update Geração de verba por RCA.sql

--- Consultas 16-05-2023/Geração de verba por RCA.sql
+++ Consultas 16-05-2023/Geração de verba por RCA.sql
@@ -1,7 +1,8 @@
 SELECT a.codusur as RCA, 
-       SUM(CASE WHEN (a.ptabela - a.punit) < 0 THEN (a.punit - a.ptabela) * a.qt ELSE 0 END) AS "Verba Gerada"
+       SUM(CASE WHEN (a.ptabela - a.punit) < 0 THEN (a.ptabela - a.punit) * a.qt ELSE 0 END) AS "Verba Gerada"
 FROM PCMOV a
 WHERE a.dtmov BETWEEN '01-mai-2023' AND SYSDATE
-  AND a.codusur = 140
+  AND a.codusur in (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
 GROUP BY a.codusur
+Order By RCA
 
