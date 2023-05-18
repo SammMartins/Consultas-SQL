d3b0bb73854135d5ca086426e43e633309e73bc9
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-18 11:20:21 -0300

    Update Separação de Carga.sql

--- Consultas 17-05-2023/Separação de Carga.sql
+++ Consultas 17-05-2023/Separação de Carga.sql
@@ -1,17 +1,31 @@
-SELECT DISTINCT a.codprod,
-       b.Descricao,
-       SUM(a.qt) qt_ped,
-       b.qtunitcx,
-       (CASE WHEN (SUM(a.qt) / b.qtunitcx) >= 1 THEN TRUNC((SUM(a.qt) / b.qtunitcx),0)||' Caixas'||' e '||TRUNC(MOD(SUM(a.qt),b.qtunitcx))||' Unidades'
+SELECT DISTINCT(a.codprod)||'' codprod, b.codfornec||'' codfornec, b.Descricao PRODUTO, 
+       SUM(a.qt) qt_ped, --b.qtunitcx "Uni. p/ Caixa",
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+       (CASE WHEN (SUM(a.qt) / b.qtunitcx) >= 1 
+             THEN TRUNC((SUM(a.qt) / b.qtunitcx),0)||' Caixas'||' e '||TRUNC(MOD(SUM(a.qt),b.qtunitcx))||' Unidades'
              Else '0 Caixas e '||SUM(a.qt) || ' Unidades' 
-        END) "Qtd. Separação",
+        END) "SEPARAÇÃO p/ CARGA",
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
        b.PESOBRUTO * SUM(a.qt) AS PESOTOTAL
        
 FROM PCPEDI a
     JOIN PCPRODUT b ON a.codprod = b.codprod
     JOIN PCPEDC c ON a.numped = c.numped
+WHERE c.numcar = 193969 --Alterar abaixo também
+GROUP BY a.codprod,b.codfornec, b.Descricao, b.qtunitcx, b.PESOBRUTO
 
-WHERE c.numcar = 193969
-
-GROUP BY a.codprod, b.Descricao, b.qtunitcx, b.PESOBRUTO
---Select * From PCPRODUT WHERE codprod = 17462
+UNION ALL 
+SELECT '_CARGA_' AS "CODPROD",
+       '_NÚMERO:_' AS "CODFORNEC",
+       c.numcar||'' AS "PRODUTO",
+       NULL AS "QT_PED",
+       --NULL AS "Uni. p/ Caixa",
+       NULL AS "SEPARAÇÃO p/ CARGA",
+       NULL AS "PESOTOTAL"
+FROM
+  PCPEDI a
+  JOIN PCPEDC c ON a.numped = c.numped
+WHERE
+  c.numcar = 193969 --Alterar acima também
+  GROUP BY c.numcar
+  Order By codfornec asc,qt_ped desc
