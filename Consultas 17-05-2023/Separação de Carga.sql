0e1ca261403df1fc6b5c8cb7c9105d07204f88cf
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-18 16:07:22 -0300

    Update Separação de Carga.sql

--- Consultas 17-05-2023/Separação de Carga.sql
+++ Consultas 17-05-2023/Separação de Carga.sql
@@ -1,37 +1,42 @@
 SELECT DISTINCT(a.codprod)||'' codprod, b.codfornec||'' codfornec, b.Descricao PRODUTO, 
        SUM(a.qt) qt_ped, --b.qtunitcx "Uni. p/ Caixa",
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    --------------------------------------------------------------------------------
        (CASE WHEN (SUM(a.qt) / b.qtunitcx) >= 1 
              THEN TRUNC((SUM(a.qt) / b.qtunitcx),0)||' Caixas'||' e '||TRUNC(MOD(SUM(a.qt),b.qtunitcx))||' Unidades'
              Else '0 Caixas e '||SUM(a.qt) || ' Unidades' 
         END) "SEPARAÇÃO p/ CARGA",
-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    --------------------------------------------------------------------------------
        b.PESOBRUTO * SUM(a.qt) AS PESOTOTAL
        
 FROM PCPEDI a
     JOIN PCPRODUT b ON a.codprod = b.codprod
     JOIN PCPEDC c ON a.numped = c.numped
-WHERE c.numcar = 193969 --Alterar abaixo também
+WHERE c.numcar = 193946 --Alterar abaixo também
+    and c.numnota in (383135,383136,383139,383140,383141,383210,383211,383212,383214,383215,383230,383231,383232,383233,383234,383243)
 GROUP BY a.codprod,b.codfornec, b.Descricao, b.qtunitcx, b.PESOBRUTO
-
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 UNION ALL
 SELECT
-  '_CARGA_' AS "CODPROD",
-  '_NÚMERO:_' AS "CODFORNEC",
-  c.numcar || '' AS "PRODUTO",
+  '  CARGA' AS "CODPROD",
+  'NÚMERO:' AS "CODFORNEC",
+  c.numcar || ' - '||'Destino: '||carg.destino AS "PRODUTO",
   NULL AS "QT_PED",
   --NULL AS "Uni. p/ Caixa",
-  '_Peso Total da Carga:_' AS "SEPARAÇÃO p/ CARGA",
-  SUM(b.PESOBRUTO * a.qt) AS PESOTOTAL
+  '  Peso Total da Carga:' AS "SEPARAÇÃO p/ CARGA",
+  Carg.totpeso AS PESOTOTAL
 FROM
   PCPEDI a
   JOIN PCPRODUT b ON a.codprod = b.codprod
   JOIN PCPEDC c ON a.numped = c.numped
+  JOIN PCCARREG Carg on c.numcar = carg.numcar
 WHERE
-  c.numcar = 193969 -- Alterar acima também
+  c.numcar = 193946 -- Alterar acima também
 GROUP BY
-  c.numcar
+  c.numcar,
+  Carg.totpeso,
+  carg.destino
 ORDER BY
   codfornec desc,
   qt_ped DESC
+  
+--SELECT * From PCPEDC where numcar = 193946 and numnota in (383135,383136,383139,383140,383141,383210,383211,383212,383214,383215,383230,383231,383232,383233,383234,383243)
