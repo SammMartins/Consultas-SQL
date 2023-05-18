7fafd2278d6d5eac5be120490937ff8b2f1fdfd4
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-18 09:55:28 -0300

    Update Separação de Carga.sql

--- Consultas 17-05-2023/Separação de Carga.sql
+++ Consultas 17-05-2023/Separação de Carga.sql
@@ -1,11 +1,17 @@
-SELECT a.codprod,
+SELECT DISTINCT a.codprod,
        b.Descricao,
+       SUM(a.qt) qt_ped,
        b.qtunitcx,
-       --(CASE WHEN (SUM(a.qt) / b.qtunitcx) >= 1 THEN (SUM(a.qt) / b.qtunitcx) || ' Caixas' Else SUM(a.qt) || ' Unidades' END) "Qtd. P/ Carga",
-       b.PESOBRUTO * SUM(a.qt) PESOTOTAL
- FROM PCPEDI a
-    JOIN PCPRODUT b on a.codprod = b.codprod
-    JOIN PCPEDC c on a.numped = c.numped
- Where c.numcar = 193969
- GROUP BY a.codprod,b.Descricao, b.unidade,b.PESOBRUTO,a.QT,b.qtunitcx
+       (CASE WHEN (SUM(a.qt) / b.qtunitcx) >= 1 THEN TRUNC((SUM(a.qt) / b.qtunitcx),0)||' Caixas'||' e '||TRUNC(MOD(SUM(a.qt),b.qtunitcx))||' Unidades'
+             Else '0 Caixas e '||SUM(a.qt) || ' Unidades' 
+        END) "Qtd. Separação",
+       b.PESOBRUTO * SUM(a.qt) AS PESOTOTAL
+       
+FROM PCPEDI a
+    JOIN PCPRODUT b ON a.codprod = b.codprod
+    JOIN PCPEDC c ON a.numped = c.numped
+
+WHERE c.numcar = 193969
+
+GROUP BY a.codprod, b.Descricao, b.qtunitcx, b.PESOBRUTO
 --Select * From PCPRODUT WHERE codprod = 17462
