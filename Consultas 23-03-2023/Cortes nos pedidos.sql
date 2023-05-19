729f2e980b94d7a3bdba1faa0d330f729691981c
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-19 13:35:08 -0300

    Update Cortes nos pedidos.sql

--- Consultas 23-03-2023/Cortes nos pedidos.sql
+++ Consultas 23-03-2023/Cortes nos pedidos.sql
@@ -1,12 +1,12 @@
 select a.codusur || ' - ' || c.nome RCA,
-       a.numped,a.codcli,a.codprod,b.descricao,TRUNC((a.qtcorte/a.qtant)*100,2) || '%' "%",a.qtant "Qt. Pedido",a.qtcorte "Qt. Corte"
+       a.numped,a.codcli,a.codprod,b.codfornec,b.descricao,TRUNC((a.qtcorte/a.qtant)*100,2) || '%' "%",a.qtant "Qt. Pedido",a.qtcorte "Qt. Corte"
     from pccortefv a
     join pcprodut b on a.codprod = b.codprod
     join pcusuari c on a.codusur = c.codusur
 where a.dtcorte > SYSDATE-1
     --and b.codfornec = 1541
-    and a.codusur in (141,140,142,153,164,157,2)
+    --and a.codusur in (141,140,142,153,164,157,2)
     --and a.codprod in (17474,17464,17467,17948,17950,17855)
 
-order by "%" asc
+order by b.codfornec desc, "Qt. Corte" desc
 
