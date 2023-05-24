e68438a792b7289b7a2eceeaecb4cc812d7be0a2
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-24 10:46:30 -0300

    Create Campanha Nutrical.sql

---
+++ Consultas 24-05-2023/Campanha Nutrical.sql
@@ -0,0 +1,19 @@
+WITH DN AS 
+    (SELECT ped.codusur AS RCA,
+       COUNT(DISTINCT ped.codcli) AS DISTINTOS
+    FROM PCPEDC ped
+        JOIN PCPEDI pedi on pedi.numped = ped.numped
+        JOIN pcprodut prod on pedi.codprod = prod.codprod
+    WHERE prod.codfornec = 1761
+        AND ped.data BETWEEN '15-mai-2023' and '15-jun-2023'
+        AND PED.DTCANCEL IS NULL
+        AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
+    GROUP BY ped.codusur)
+    
+SELECT  usur.codusur|| ' - ' || usur.nome AS "Vendedor",
+        dist.distintos AS "DN DISTINTOS"
+        
+FROM PCUSUARI usur
+    JOIN DN dist ON usur.codusur = dist.RCA
+WHERE usur.codusur IN (140,141,142,157,164,153,158,155,156,167,169,170,172,151)
+ORDER BY "DN DISTINTOS" DESC
