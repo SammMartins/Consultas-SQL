db12413850e33c0c05a1a3d0eb2420411c4ecf05
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-24 10:50:21 -0300

    Update Campanha Nutrical.sql

--- Consultas 24-05-2023/Campanha Nutrical.sql
+++ Consultas 24-05-2023/Campanha Nutrical.sql
@@ -9,11 +9,9 @@ WITH DN AS
         AND PED.DTCANCEL IS NULL
         AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
     GROUP BY ped.codusur)
-    
-SELECT  usur.codusur|| ' - ' || usur.nome AS "Vendedor",
-        dist.distintos AS "DN DISTINTOS"
-        
+----------------------------------------------------------------------------------------------------------------------------    
+SELECT  usur.codusur|| ' - ' || usur.nome AS "Vendedor", dist.distintos AS "DN DISTINTOS"        
 FROM PCUSUARI usur
     JOIN DN dist ON usur.codusur = dist.RCA
-WHERE usur.codusur IN (140,141,142,157,164,153,158,155,156,167,169,170,172,151)
+WHERE usur.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
 ORDER BY "DN DISTINTOS" DESC
