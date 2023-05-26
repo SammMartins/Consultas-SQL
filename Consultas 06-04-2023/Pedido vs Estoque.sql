23a96bdaab6153fbab5ce19540561d578a5c23a7
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-26 10:12:19 -0300

    Update Pedido vs Estoque.sql

--- Consultas 06-04-2023/Pedido vs Estoque.sql
+++ Consultas 06-04-2023/Pedido vs Estoque.sql
@@ -4,10 +4,12 @@ SELECT Distinct a.CODPROD, c.descricao,
 from pcpedi a
     JOIN PCest b on a.codprod = b.codprod
     join pcprodut c on a.codprod = c.codprod
-WHERE a.DATA > '02-abr-2023'
+WHERE a.DATA > SYSDATE - 5
     --AND a.CODUSUR In (140,141,142,143,153,164,157,2,10)
     AND a.posicao != 'F' AND a.posicao != 'C'
     AND (a.QT - b.qtest) > 0
+    --AND a.codprod = 17443
+    AND a.posicao NOT LIKE 'C'
     and b.codfilial = 3
     --AND c.descricao like '%YOPRO%'
 group by a.codprod,c.descricao,b.qtest,b.qtreserv
