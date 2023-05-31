8a15e5905c0ca4e3e9be3b123be6aa1217b66f29
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-31 11:29:01 -0300

    Update Cortes nos pedidos.sql

--- Consultas 23-03-2023/Cortes nos pedidos.sql
+++ Consultas 23-03-2023/Cortes nos pedidos.sql
@@ -4,7 +4,7 @@ select a.codusur || ' - ' || c.nome RCA,
     join pcprodut b on a.codprod = b.codprod
     join pcusuari c on a.codusur = c.codusur
 where a.dtcorte > SYSDATE-1
-    --and b.codfornec = 1541
+    --and b.codfornec = 1607
     --and a.codusur in (141,140,142,153,164,157,2)
     --and a.codprod in (17474,17464,17467,17948,17950,17855)
 
