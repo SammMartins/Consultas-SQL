c83681168418cc4f800233823ff2441a8fca1add
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-05-31 11:28:44 -0300

    Update Campanha Nutrical.sql

--- Consultas 24-05-2023/Campanha Nutrical.sql
+++ Consultas 24-05-2023/Campanha Nutrical.sql
@@ -1,27 +1,29 @@
 SELECT * FROM
-(WITH DN AS 
-    (SELECT ped.codusur AS RCA,
-       COUNT(DISTINCT ped.codcli) AS DISTINTOS
-    FROM PCPEDC ped
-        JOIN PCPEDI pedi on pedi.numped = ped.numped
-        JOIN pcprodut prod on pedi.codprod = prod.codprod
-    WHERE prod.codfornec = 1761
-        AND ped.data BETWEEN '15-mai-2023' and '15-jun-2023'
-        AND PED.DTCANCEL IS NULL
-        AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
-    GROUP BY ped.codusur)
+
+    (WITH DN AS 
+        (SELECT ped.codusur AS RCA,
+           COUNT(DISTINCT ped.codcli) AS DISTINTOS
+        FROM PCPEDC ped
+            JOIN PCPEDI pedi on pedi.numped = ped.numped
+            JOIN pcprodut prod on pedi.codprod = prod.codprod
+        WHERE prod.codfornec = 1761
+            AND ped.data BETWEEN '15-mai-2023' and '15-jun-2023'
+            AND PED.DTCANCEL IS NULL
+            AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
+        GROUP BY ped.codusur)
 ----------------------------------------------------------------------------------------------------------------------------    
-SELECT  usur.codusur "COD", usur.nome AS "Vendedor", dist.distintos AS "DN DISTINTOS"        
-FROM PCUSUARI usur
-    JOIN DN dist ON usur.codusur = dist.RCA
-WHERE usur.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+    SELECT  usur.codusur "COD", usur.nome AS "Vendedor", dist.distintos AS "DN DISTINTOS"        
+    FROM PCUSUARI usur
+        JOIN DN dist ON usur.codusur = dist.RCA
+    WHERE usur.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
 
 ----------------------------------------------------------------------------------------------------------------------------    
-UNION all
-SELECT NULL as "COD",
-       'TOTAL DE POSITIVAÇÕES' as "Vendedor",
-       SUM(a.distintos) AS "DN DISTINTOS"
-FROM DN a)
+    UNION all
+    SELECT NULL as "COD",
+           'TOTAL DE POSITIVAÇÕES' as "Vendedor",
+           SUM(a.distintos) AS "DN DISTINTOS"
+    FROM DN a)
+    
 ORDER BY
-    CASE WHEN "COD" IS NULL THEN 1 ELSE 0 END,
-    "DN DISTINTOS" DESC
+CASE WHEN "COD" IS NULL THEN 1 ELSE 0 END,
+"DN DISTINTOS" DESC
