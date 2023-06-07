05b759abcc188dd6a38fc1cfa316947ad696e326
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-07 16:18:59 -0300

    Update Campanha Nutrical.sql

--- Consultas 24-05-2023/Campanha Nutrical.sql
+++ Consultas 24-05-2023/Campanha Nutrical.sql
@@ -7,7 +7,7 @@ SELECT * FROM
             JOIN PCPEDI pedi on pedi.numped = ped.numped
             JOIN pcprodut prod on pedi.codprod = prod.codprod
         WHERE prod.codfornec = 1761
-            AND ped.data BETWEEN '15-mai-2023' and '15-jun-2023'
+            AND ped.data BETWEEN '15-may-2023' and '15-jun-2023'
             AND PED.DTCANCEL IS NULL
             AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
         GROUP BY ped.codusur)
@@ -15,7 +15,7 @@ SELECT * FROM
     SELECT  usur.codusur "COD", usur.nome AS "Vendedor", dist.distintos AS "DN DISTINTOS"        
     FROM PCUSUARI usur
         JOIN DN dist ON usur.codusur = dist.RCA
-    WHERE usur.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+    WHERE usur.codusur IN (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 161, 164, 167, 168, 169, 170, 172, 174)
 
 ----------------------------------------------------------------------------------------------------------------------------    
     UNION all
