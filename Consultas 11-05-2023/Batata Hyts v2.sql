b0458852bb6796f521699b7212091d5e7e700fc9
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-07 16:18:52 -0300

    Update Batata Hyts v2.sql

--- Consultas 11-05-2023/Batata Hyts v2.sql
+++ Consultas 11-05-2023/Batata Hyts v2.sql
@@ -5,7 +5,7 @@ WITH PC_Hyts AS (
            SUM(CASE WHEN d.CODPROD IN (18182) THEN d.qt ELSE 0 END) AS unidades_palha,
            SUM(CASE WHEN d.CODPROD IN (18364, 18365) THEN d.qt ELSE 0 END) AS unidades_puruca
     FROM pcpedi d
-    WHERE d.data BETWEEN '1-mai-2023' AND '31-mai-2023'
+    WHERE d.data BETWEEN '1-jun-2023' AND '30-jun-2023'
         AND d.posicao NOT LIKE 'C'
         AND d.vlbonific = 0
     GROUP BY d.codusur)
@@ -17,7 +17,7 @@ SELECT (u.codusur || ' - ' || u.nome) AS "Vendedor",
        ((c.unidades_chips * 2) + (c.unidades_palha * 3) + (c.unidades_puruca * 2)) / 24 AS "R$ Total"
 FROM PCUSUARI u
     JOIN PC_Hyts c ON u.codusur = c.codusur
-WHERE u.codusur IN (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+WHERE u.codusur IN (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
 ----------------------------------------------------------------------------------------------------------------------------------------
 UNION ALL --Utilizada para realizar a união de duas consultas, combinando os resultados em um único conjunto de resultados
 SELECT 
@@ -27,5 +27,5 @@ SELECT
     SUM(c.unidades_puruca / 24) AS "Caixas de PURUCA 70g",
     SUM(((c.unidades_chips * 2) + (c.unidades_palha * 3) + (c.unidades_puruca * 2)) / 24) AS "R$ Total"   
 FROM PC_Hyts c
-    where c.codusur in (140, 141, 142, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 164, 167, 168, 169, 170, 172, 174)
+    where c.codusur in (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 161, 164, 167, 168, 169, 170, 172, 174)
 Order By "R$ Total"
