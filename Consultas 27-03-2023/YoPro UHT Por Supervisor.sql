afacaed2cb8e868b4d6a7a13b85242de38370a35
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-09 18:03:29 -0300

    Update YoPro UHT Por Supervisor.sql

--- Consultas 27-03-2023/YoPro UHT Por Supervisor.sql
+++ Consultas 27-03-2023/YoPro UHT Por Supervisor.sql
@@ -1,43 +1,49 @@
+WITH addyopro AS 
+( SELECT 0 AS ADDJR,0 AS ADDDIDA FROM DUAL)
+
+
                                     --YoPro UHT: 17883, 17884, 17885, 18173
 SELECT 
     DISTINCT a.codsupervisor || ' - ' || s.nome "Supervisor",
 --------------------------------------------------------------------------------------------------------------------
-    (SELECT SUM(qt)/24 
-     FROM pcpedi c
-     JOIN pcusuari d ON c.codusur = d.codusur
+    (SELECT (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END )
+     FROM pcpedi c, addyopro ad, pcusuari d
      WHERE c.codprod IN (17883,17884,17885,18173)
        AND c.pvenda < 6.90
-       AND c.DATA BETWEEN '1-mai-2023' AND SYSDATE
+       AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
        AND d.codsupervisor = a.codsupervisor
        AND c.vlbonific = 0
+       AND c.codusur = d.codusur
+       Group BY d.codsupervisor,ad.adddida,ad.addjr
     ) AS "Qt. Caixas",
 --------------------------------------------------------------------------------------------------------------------
   ROUND( 
-    ((SELECT SUM(qt)/24 
-       FROM pcpedi c
-       JOIN pcusuari d ON c.codusur = d.codusur
+    ((SELECT (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END )
+       FROM pcpedi c, addyopro ad, pcusuari d
        WHERE c.codprod IN (17883,17884,17885,18173)
          AND c.pvenda < 6.90
-         AND c.DATA BETWEEN '1-mai-2023' AND SYSDATE
+         AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
          AND c.vlbonific = 0
-         AND d.codsupervisor = a.codsupervisor) 
+         AND c.codusur = d.codusur
+         AND d.codsupervisor = a.codsupervisor
+         Group BY d.codsupervisor,ad.adddida,ad.addjr) 
             / 
-          (SELECT SUM(qt)/24 
-           FROM pcpedi c
-           JOIN pcusuari d ON c.codusur = d.codusur
+          (SELECT (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END )
+           FROM pcpedi c, addyopro ad, pcusuari d
            WHERE c.codprod IN (17883,17884,17885,18173)
              AND c.pvenda < 6.90
-             AND c.DATA BETWEEN '1-mai-2023' AND SYSDATE
+             AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
              AND c.vlbonific = 0
+             AND c.codusur = d.codusur
              AND d.codsupervisor IN (2,8,9)
-          )) * 100 
+          Group BY d.codsupervisor,ad.adddida,ad.addjr)) * 100 
 ,1) AS "%"
 --------------------------------------------------------------------------------------------------------------------
 FROM pcusuari a join PCSUPERV s on a.codsupervisor = s.codsupervisor
-WHERE a.codsupervisor IN (2,8,9)
+WHERE a.codsupervisor IN (2,8)
 
 ----------------------------------------------------------------------------------------------------------------------------------------
-UNION ALL --União das 3 linhas com a 4ª linha com o valor total
+UNION ALL --União das 2 linhas com a 4ª linha com o valor total
 ----------------------------------------------------------------------------------------------------------------------------------------
 SELECT 
     'Total' AS "Supervisor",
@@ -47,8 +53,8 @@ FROM pcpedi c
 JOIN pcusuari d ON c.codusur = d.codusur
 WHERE c.codprod IN (17883,17884,17885,18173)
   AND c.pvenda < 6.90
-  AND c.DATA BETWEEN '1-mai-2023' AND SYSDATE
+  AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
   AND c.vlbonific = 0
-  AND d.codsupervisor IN (2,8,9) 
+  AND d.codsupervisor IN (2,8) 
   
 Order By "Qt. Caixas"
