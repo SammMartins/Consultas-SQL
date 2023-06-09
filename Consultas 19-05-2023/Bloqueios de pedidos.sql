15b67cad835ca66641113b5a59879be8d3db9c38
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-09 18:03:32 -0300

    Update Bloqueios de pedidos.sql

--- Consultas 19-05-2023/Bloqueios de pedidos.sql
+++ Consultas 19-05-2023/Bloqueios de pedidos.sql
@@ -7,9 +7,8 @@ FROM pcpedi a
     JOIN pcclient d on d.codcli = a.codcli --SELECT * FROM PCclient
     
 WHERE a.data BETWEEN SYSDATE-7 and SYSDATE
-    AND a.codusur in (141,140,142,157) --or a.codusur in (153,164,143)
-                     --(158,155,156,167,169,170,172)
-                     --(148,145,150,151,152,168,174)
+    AND a.codusur in --(148,145,150,151,152,168,174)  --or a.codusur in (141,140,142,157) --or a.codusur in (153,164,143)
+                     (158,155,156,167,169,170,172)
     AND a.posicao Not Like 'F' AND a.posicao Not Like 'M' 
     --AND a.posicao Not Like 'L' 
 ORDER BY CODCLI,NUMPED
