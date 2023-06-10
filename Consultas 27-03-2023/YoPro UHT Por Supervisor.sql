c4f50a45c123d9346fadd4e1b66c7249696d0a77
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-10 11:05:28 -0300

    Update YoPro UHT Por Supervisor.sql

--- Consultas 27-03-2023/YoPro UHT Por Supervisor.sql
+++ Consultas 27-03-2023/YoPro UHT Por Supervisor.sql
@@ -1,54 +1,39 @@
-WITH addyopro AS 
-( SELECT 0 AS ADDJR,0 AS ADDDIDA FROM DUAL)
+WITH addyopro AS (SELECT 0 AS ADDJR, 0 AS ADDDIDA FROM DUAL),
+qt_caixas AS (
+  SELECT 
+    a.codsupervisor || ' - ' || s.nome AS "Supervisor",
+    (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END) AS "Qt. Caixas"
+  FROM pcusuari a
+  JOIN PCSUPERV s ON a.codsupervisor = s.codsupervisor
+  JOIN pcpedi c ON c.codusur = a.codusur
+  JOIN addyopro ad ON 1 = 1
+  JOIN pcusuari d ON c.codusur = d.codusur
+  WHERE a.codsupervisor IN (2, 8)
+    AND c.codprod IN (17883, 17884, 17885, 18173)
+    AND c.pvenda < 6.90
+    AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
+    AND c.vlbonific = 0
+    AND d.codsupervisor = a.codsupervisor
+  GROUP BY a.codsupervisor, s.nome, d.codsupervisor, ad.adddida, ad.addjr
+)
+SELECT
+  "Supervisor",
+  "Qt. Caixas",
+  ROUND(
+    ("Qt. Caixas" / (
+      SELECT SUM(qt_caixas."Qt. Caixas")
+      FROM qt_caixas
+      WHERE qt_caixas."Supervisor" != 'Total'
+    )) * 100, 1
+  ) AS "%"
+FROM qt_caixas
 
+UNION ALL
 
-                                    --YoPro UHT: 17883, 17884, 17885, 18173
 SELECT 
-    DISTINCT a.codsupervisor || ' - ' || s.nome "Supervisor",
---------------------------------------------------------------------------------------------------------------------
-    (SELECT (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END )
-     FROM pcpedi c, addyopro ad, pcusuari d
-     WHERE c.codprod IN (17883,17884,17885,18173)
-       AND c.pvenda < 6.90
-       AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
-       AND d.codsupervisor = a.codsupervisor
-       AND c.vlbonific = 0
-       AND c.codusur = d.codusur
-       Group BY d.codsupervisor,ad.adddida,ad.addjr
-    ) AS "Qt. Caixas",
---------------------------------------------------------------------------------------------------------------------
-  ROUND( 
-    ((SELECT (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END )
-       FROM pcpedi c, addyopro ad, pcusuari d
-       WHERE c.codprod IN (17883,17884,17885,18173)
-         AND c.pvenda < 6.90
-         AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
-         AND c.vlbonific = 0
-         AND c.codusur = d.codusur
-         AND d.codsupervisor = a.codsupervisor
-         Group BY d.codsupervisor,ad.adddida,ad.addjr) 
-            / 
-          (SELECT (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END )
-           FROM pcpedi c, addyopro ad, pcusuari d
-           WHERE c.codprod IN (17883,17884,17885,18173)
-             AND c.pvenda < 6.90
-             AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
-             AND c.vlbonific = 0
-             AND c.codusur = d.codusur
-             AND d.codsupervisor IN (2,8,9)
-          Group BY d.codsupervisor,ad.adddida,ad.addjr)) * 100 
-,1) AS "%"
---------------------------------------------------------------------------------------------------------------------
-FROM pcusuari a join PCSUPERV s on a.codsupervisor = s.codsupervisor
-WHERE a.codsupervisor IN (2,8)
-
-----------------------------------------------------------------------------------------------------------------------------------------
-UNION ALL --União das 2 linhas com a 4ª linha com o valor total
-----------------------------------------------------------------------------------------------------------------------------------------
-SELECT 
-    'Total' AS "Supervisor",
-    SUM(qt)/24 AS "Qt. Caixas",
-    100.00 AS "%"  -- Valor fixo de 100,00 para a linha de total
+  'Total' AS "Supervisor",
+  SUM(qt)/24 AS "Qt. Caixas",
+  100.00 AS "%"  
 FROM pcpedi c
 JOIN pcusuari d ON c.codusur = d.codusur
 WHERE c.codprod IN (17883,17884,17885,18173)
@@ -56,5 +41,5 @@ WHERE c.codprod IN (17883,17884,17885,18173)
   AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
   AND c.vlbonific = 0
   AND d.codsupervisor IN (2,8) 
-  
-Order By "Qt. Caixas"
+
+ORDER BY "Qt. Caixas";
