acd1ce607682eb95c78f0c0e46816035e91487a2
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-13 09:20:48 -0300

    Create Danila.sql

---
+++ Consultas 12-06-2023/Danila.sql
@@ -0,0 +1,9 @@
+SELECT a.DATA,a.CODPROD,d.CODSUPERVISOR,a.CODUSUR,a.CODCLI,c.CLIENTE,c.ENDERENT,c.BAIRROENT,
+       c.MUNICENT,b.CODFORNEC,a.QT,a.PVENDA,a.QT*a.PVENDA TOTAL,b.CODSEC,b.QTUNITCX
+ 
+FROM PCPEDI a
+JOIN PCPRODUT b on a.codprod = b.codprod
+JOIN pcclient c on a.codcli = c.codcli
+JOIN PCusuari d on a.CODUSUR = d.CODUSUR
+
+
