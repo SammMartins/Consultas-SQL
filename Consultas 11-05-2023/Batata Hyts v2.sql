f55013b1e7f7975459e6e09f4bd16281fe288b94
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-15 10:04:05 -0300

    Update Batata Hyts v2.sql

--- Consultas 11-05-2023/Batata Hyts v2.sql
+++ Consultas 11-05-2023/Batata Hyts v2.sql
@@ -21,7 +21,7 @@ WHERE u.codusur IN (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 156, 
 ----------------------------------------------------------------------------------------------------------------------------------------
 UNION ALL --Utilizada para realizar a união de duas consultas, combinando os resultados em um único conjunto de resultados
 SELECT 
-    ' - DESEMPENHO TOTAL DA EQUIPE' AS "Vendedor",
+    '    - DESEMPENHO TOTAL DA EQUIPE' AS "Vendedor",
     SUM(c.unidades_chips / 24) AS "Caixas de CHIPS 40g",
     SUM(c.unidades_palha / 24) AS "Caixas de PALHA 70g",
     SUM(c.unidades_puruca / 24) AS "Caixas de PURUCA 70g",
