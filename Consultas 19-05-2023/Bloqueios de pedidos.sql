ba35842f0057a1dd07733b1896698471aaa30cc9
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-19 09:36:16 -0300

    Update Bloqueios de pedidos.sql

--- Consultas 19-05-2023/Bloqueios de pedidos.sql
+++ Consultas 19-05-2023/Bloqueios de pedidos.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT(a.NUMPED)"Nº PEDIDO",'R$'||REPLACE(b.vltotal, '.', ',') "R$",a.codcli|| ' - ' ||d.cliente Cliente,
-                a.codusur rca,(CASE WHEN a.posicao like 'B' Then a.posicao||'loqueado' ELSE a.posicao||'iberado' END) Status,
+                a.codusur rca,(CASE WHEN a.posicao like 'B' Then a.posicao||'loqueado' WHEN a.posicao like 'C' Then a.posicao||'ancelado' ELSE a.posicao||'iberado' END) Status,
                 TO_CHAR(a.data, 'DD/MM') || ' às ' || b.hora||'H' Envio,
                 b.obs "MOTIVO ANALISADO",
                 (SELECT LISTAGG(c.motivo, ' | ') WITHIN GROUP (ORDER BY c.motivo desc)FROM PCBLOQUEIOSPEDIDO c WHERE a.numped = c.numped) "MOTIVO SISTEMA"
@@ -10,9 +10,9 @@ FROM pcpedi a
     JOIN pcclient d on d.codcli = a.codcli 
     
 WHERE a.data BETWEEN SYSDATE-7 and SYSDATE
-    AND a.codusur in --(153,143,148,145,150,151,152,168,174)  
-                     --(141,140,142,157,2) 
-                     (158,155,156,167,169,170,172)
+    AND a.codusur in --(153,143,148,145,150,151,152,168,174)
+                     (141,140,142,157)
+                     --(158,155,156,167,169,170,172)
     AND a.posicao Not Like 'F' AND a.posicao Not Like 'M' 
     --AND a.posicao Not Like 'L' 
 ORDER BY "Nº PEDIDO"
