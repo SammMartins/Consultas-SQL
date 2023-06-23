230e65d3e167f0acd6bf346cd2a60791372d7a2d
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-23 14:32:53 -0300

    Update Inadiplencia v2.sql

--- Consultas 16-01-2023/Inadiplencia v2.sql
+++ Consultas 16-01-2023/Inadiplencia v2.sql
@@ -1,7 +1,7 @@
 SELECT A.codusur RCA,A.codcli || ' ' || B.CLIENTE CLIENTE, 'R$' || REPLACE(round(A.valor,2), '.', ',') Título, 
-       'R$' || REPLACE(round((A.valor * 0.00333) * round(SYSDATE-A.dtvenc,0),2), '.', ',') Juros_Aproximado, 
+       'R$' || REPLACE(round((A.valor * 0.00333) * round(SYSDATE-A.dtvenc,0),2), '.', ',') "JUROS APROXIMADOS", 
        'R$' || REPLACE(round(A.valor + ((A.valor * 0.00333) * round(SYSDATE-A.dtvenc,0)), 2), '.', ',') Total,
-       A.codcob COB, round(SYSDATE-A.dtvenc,0)  DIAS_VENCIMENTO, 
+       A.codcob COB, round(SYSDATE-A.dtvenc,0)  "DIAS VENCIMENTO", 
        A.dtemissao EMISSAO,A.dtvenc VENCIMENTO
        
        
@@ -10,8 +10,7 @@ join pcclient B on A.codcli = B.codcli
 
 where A.dtvenc between '01-jan-2020' and SYSDATE-6 --'{DATAINI}' AND '{DATAFIN}'
 and A.vpago is NULL
-and A.codusur in (140,141,142,145,148,149,150,151,152,153,155,156,
-                    157,158,160,164,167,168,169,170,172,174) --({VENDEDOR})
+and A.codusur in (156) --({VENDEDOR})
 and A.codcob in ('7563','SERA','C','CHD1')
 
 order by DIAS_VENCIMENTO Desc, TÍTULO Desc
