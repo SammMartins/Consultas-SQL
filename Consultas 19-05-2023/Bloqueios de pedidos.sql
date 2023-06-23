fd07749ee0b682f0c39c4148dadfb07acb86470f
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-23 14:32:29 -0300

    Update Bloqueios de pedidos.sql

--- Consultas 19-05-2023/Bloqueios de pedidos.sql
+++ Consultas 19-05-2023/Bloqueios de pedidos.sql
@@ -1,7 +1,6 @@
-SELECT DISTINCT(a.NUMPED)"Nº PEDIDO",'R$'||REPLACE(b.vltotal, '.', ',') "R$",a.codcli|| ' - ' ||d.cliente Cliente,
-                a.codusur rca,(CASE WHEN a.posicao like 'B' Then a.posicao||'loqueado' WHEN a.posicao like 'C' Then a.posicao||'ancelado' ELSE a.posicao||'iberado' END) Status,
-                TO_CHAR(a.data, 'DD/MM') || ' às ' || b.hora||'H' Envio,
-                b.obs "MOTIVO ANALISADO",
+SELECT DISTINCT(a.NUMPED)"Nº PEDIDO", TO_CHAR(a.data, 'DD/MM') || ' às ' || b.hora||'H' Envio,'R$'||REPLACE(b.vltotal, '.', ',') "Valor R$",
+                (CASE WHEN a.posicao like 'B' Then a.posicao||'loqueado' WHEN a.posicao like 'C' Then a.posicao||'ancelado' ELSE a.posicao||'iberado' END) Status,
+                ' ' " ",a.codcli|| ' - ' ||d.cliente Cliente,a.codusur rca,' ' "  ",b.obs "MOTIVO ANALISADO",
                 (SELECT LISTAGG(c.motivo, ' | ') WITHIN GROUP (ORDER BY c.motivo desc)FROM PCBLOQUEIOSPEDIDO c WHERE a.numped = c.numped) "MOTIVO SISTEMA"
                 
 FROM pcpedi a
@@ -11,9 +10,9 @@ FROM pcpedi a
     
 WHERE a.data BETWEEN SYSDATE-7 and SYSDATE
     AND a.codusur in --(153,143,148,145,150,151,152,168,174)
-                     (141,140,142,157)
-                     --(158,155,156,167,169,170,172)
-    --(153,140,141,142,145,148,150,151,152,155,156,157,158,167,168,169,170,172,174) AND (SYSDATE - a.DATA > 3) and  (SYSDATE - a.DATA < 30)
+                     --(141,140,142,157)
+                     --(158,155,156,167,161,169,170,172)
+    (143,161,153,140,141,142,145,148,150,151,152,155,156,157,158,167,168,169,170,172,174) --AND (SYSDATE - a.DATA > 3) and  (SYSDATE - a.DATA < 30)
     AND a.posicao Not Like 'F' AND a.posicao Not Like 'M' 
     --AND a.posicao Not Like 'L' 
     
