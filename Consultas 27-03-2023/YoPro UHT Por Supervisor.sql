26449dcb6a0b9c75a22040137a7afdad94f74b80
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-27 10:17:37 -0300

    Update YoPro UHT Por Supervisor.sql

--- Consultas 27-03-2023/YoPro UHT Por Supervisor.sql
+++ Consultas 27-03-2023/YoPro UHT Por Supervisor.sql
@@ -1,9 +1,9 @@
-WITH addyopro AS (SELECT 0 AS ADDJR, 14 AS ADDDIDA FROM DUAL), -- Adição manual de Caixas com desconto no boleto
-------------------------------------------------------------------------------------------------------------------------    
-qt_caixas AS 
-(SELECT 
-    a.codsupervisor || ' - ' || s.nome AS "Supervisor",
-    (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END) AS "Qt. Caixas"
+WITH addyopro AS (SELECT 0 AS ADDJR, 14 AS ADDDIDA, 300 AS GAP FROM DUAL),
+qt_caixas AS (
+  SELECT
+    a.codsupervisor || ' - ' || SUBSTR(s.nome,0,15) AS "Supervisor",
+    (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END) AS "Cx. Vendidas",
+    ad.gap - (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END) AS GAP
   FROM pcusuari a
   JOIN PCSUPERV s ON a.codsupervisor = s.codsupervisor
   JOIN pcpedi c ON c.codusur = a.codusur
@@ -12,36 +12,33 @@ qt_caixas AS
   WHERE a.codsupervisor IN (2, 8)
     AND c.codprod IN (17883, 17884, 17885, 18173)
     AND c.pvenda < 6.90
-    AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
+    AND c.DATA BETWEEN '01-jun-2023' AND SYSDATE
     AND c.vlbonific = 0
     AND d.codsupervisor = a.codsupervisor
-  GROUP BY a.codsupervisor, s.nome, d.codsupervisor, ad.adddida, ad.addjr)
---------------------------------------------------------------------------
+  GROUP BY a.codsupervisor, s.nome, d.codsupervisor, ad.adddida, ad.addjr,ad.gap
+)
 SELECT
   "Supervisor",
-  "Qt. Caixas",
-  ROUND(
-    ("Qt. Caixas" / (
-      SELECT SUM(qt_caixas."Qt. Caixas")
-      FROM qt_caixas
-      WHERE qt_caixas."Supervisor" != 'Total'
-    )) * 100, 0
-  ) AS "%"
+  -GAP AS GAP,
+  "Cx. Vendidas",
+  ROUND(("Cx. Vendidas" / (SELECT SUM(qt_caixas."Cx. Vendidas") FROM qt_caixas WHERE qt_caixas."Supervisor" != '  - Total')) * 100, 0) AS "%"
 FROM qt_caixas
-------------------------------------------------------------------------------------------------------------------------
+
 UNION ALL
 
-SELECT 
-  '  - Total' AS "Supervisor",
-  (SUM(qt)/24) + ad.adddida + ad.addjr AS "Qt. Caixas",
-  100.00 AS "%"  
+SELECT
+  '  - TOTAL' AS "Supervisor",
+  -((ad.gap*2) - ((SUM(qt)/24) + ad.adddida + ad.addjr)) AS GAP,
+  (SUM(qt)/24) + ad.adddida + ad.addjr AS "Cx. Vendidas",
+  100.00 AS "%"
 FROM pcpedi c
 JOIN pcusuari d ON c.codusur = d.codusur
 JOIN addyopro ad ON 1 = 1
 WHERE c.codprod IN (17883,17884,17885,18173)
   AND c.pvenda < 6.90
-  AND c.DATA BETWEEN '1-jun-2023' AND SYSDATE
+  AND c.DATA BETWEEN '01-jun-2023' AND SYSDATE
   AND c.vlbonific = 0
-  AND d.codsupervisor IN (2,8) 
+  AND d.codsupervisor IN (2,8)
+GROUP BY ad.adddida, ad.addjr,ad.gap
+ORDER BY "Cx. Vendidas";
 
-ORDER BY "Qt. Caixas"
