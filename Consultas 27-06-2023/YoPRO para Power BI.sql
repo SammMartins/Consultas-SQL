f44a79001e8fd60a7170c3dbce6eac89cacde832
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-06-27 15:31:03 -0300

    Create YoPRO para Power BI.sql

---
+++ Consultas 27-06-2023/YoPRO para Power BI.sql
@@ -0,0 +1,28 @@
+WITH addyopro AS (SELECT 0 AS ADDJR, 14 AS ADDDIDA, 300 AS GAP FROM DUAL),
+qt_caixas AS (
+  SELECT
+    a.codsupervisor || ' - ' || SUBSTR(s.nome,0,15) AS "Supervisor",
+    (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END) AS "Cx. Vendidas",
+    ad.gap - (CASE WHEN d.codsupervisor = 2 THEN (SUM(c.qt)/24)+ad.adddida ELSE (SUM(c.qt)/24)+ad.addjr END) AS GAP
+  FROM pcusuari a
+  JOIN PCSUPERV s ON a.codsupervisor = s.codsupervisor
+  JOIN pcpedi c ON c.codusur = a.codusur
+  JOIN addyopro ad ON 1 = 1
+  JOIN pcusuari d ON c.codusur = d.codusur
+  WHERE a.codsupervisor IN (2, 8)
+    AND c.codprod IN (17883, 17884, 17885, 18173)
+    AND c.pvenda < 6.90
+    AND c.DATA BETWEEN '01-jun-2023' AND SYSDATE
+    AND c.vlbonific = 0
+    AND d.codsupervisor = a.codsupervisor
+  GROUP BY a.codsupervisor, s.nome, d.codsupervisor, ad.adddida, ad.addjr,ad.gap
+)
+SELECT
+  "Supervisor",
+  -GAP AS GAP,
+  "Cx. Vendidas",
+  ROUND(("Cx. Vendidas" / (SELECT SUM(qt_caixas."Cx. Vendidas") FROM qt_caixas WHERE qt_caixas."Supervisor" != '  - Total')) * 100, 0)||'%' AS "%",
+  300 AS ALVO
+FROM qt_caixas
+ORDER BY "Supervisor"
+
