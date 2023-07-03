8f740d174092c0fd5e89560c8cd913861bd7fbdf
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-07-03 10:27:00 -0300

    Update Consulta para Power BI.sql

--- Power BI/Consulta para Power BI.sql
+++ Power BI/Consulta para Power BI.sql
@@ -1,7 +1,10 @@
 SELECT
 ped.codusur,
 usur.codsupervisor,
+ped.codcli,
+cli.cliente,--select * from pcclient
 ped.codprod,
+prod.descricao,
 ped.numped,
 prod.codfornec,
 prod.codsec,
@@ -13,6 +16,7 @@ ped.posicao
 from pcpedi ped
 join pcprodut prod on ped.codprod = prod.codprod
 join pcusuari usur on ped.codusur = usur.codusur
+join pcclient cli on ped.codcli = cli.codcli
 WHERE ped.data > SYSDATE-90
 
 --select * from pcprodut
