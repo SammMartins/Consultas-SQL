494f152c55391d565d8288e7d9900f994852f88b
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-07-06 14:58:20 -0300

    Update Campanha Combos Danone.sql

--- Consultas 06-07-2023/Campanha Combos Danone.sql
+++ Consultas 06-07-2023/Campanha Combos Danone.sql
@@ -1,15 +1,25 @@
 WITH Contagem AS
-(SELECT COUNT(DISTINCT(p.numped)) cont,p.codusur rca,
-LISTAGG(p.codcli, '- ') WITHIN GROUP (ORDER BY p.codcli) clientes
-FROM PCPEDI p
-WHERE p.CODCOMBO in (118,119,120) 
-AND p.data BETWEEN '01-jul-2023' AND SYSDATE
-GROUP BY p.codusur)
-----------------------------------------------------------------------------------------------------------------------------------------
-SELECT (u.codusur || ' - ' || u.nome) AS "Vendedor"
-        ,'R$'||c.cont"R$"
-        ,c.clientes
-       
+    (SELECT
+        COUNT(DISTINCT p.numped) AS cont,
+        p.codusur AS rca,
+        LISTAGG(p.codcli, ' - ') WITHIN GROUP (ORDER BY p.codcli) AS clientes
+    FROM
+        (SELECT
+            p.*,
+            ROW_NUMBER() OVER (PARTITION BY p.codusur, p.codcli ORDER BY p.codcli) AS rn
+        FROM PCPEDI p
+        WHERE p.CODCOMBO IN (118, 119, 120) 
+        AND p.data BETWEEN '01-jul-2023' AND SYSDATE) p
+        
+    WHERE p.rn = 1
+    GROUP BY p.codusur
+)
+
+SELECT
+    (u.codusur || ' - ' || u.nome) AS "Vendedor",
+    'R$'|| (CASE WHEN c.cont IS NULL THEN 0 ELSE c.cont END) AS "R$",
+    c.clientes "Clientes Atendidos"
 FROM PCUSUARI u
-    JOIN Contagem c ON u.codusur = c.rca
+LEFT JOIN Contagem c ON u.codusur = c.rca
 WHERE u.codusur IN (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 161, 164, 167, 168, 169, 170, 172, 174)
+ORDER BY "R$" desc, "Vendedor" ASC
