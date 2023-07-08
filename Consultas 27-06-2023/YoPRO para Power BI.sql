826751a60ae247089d3e194666032af5db6075f4
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-07-08 12:23:41 -0300

    Update YoPRO para Power BI.sql

--- Consultas 27-06-2023/YoPRO para Power BI.sql
+++ Consultas 27-06-2023/YoPRO para Power BI.sql
@@ -1,4 +1,4 @@
-WITH addyopro AS (SELECT 0 AS ADDJR, 14 AS ADDDIDA, 300 AS GAP FROM DUAL),
+WITH addyopro AS (SELECT 0 AS ADDJR, 0 AS ADDDIDA, 300 AS GAP FROM DUAL),
 qt_caixas AS (
   SELECT
     a.codsupervisor || ' - ' || SUBSTR(s.nome,0,15) AS "Supervisor",
@@ -12,7 +12,7 @@ qt_caixas AS (
   WHERE a.codsupervisor IN (2, 8)
     AND c.codprod IN (17883, 17884, 17885, 18173)
     AND c.pvenda < 6.90
-    AND c.DATA BETWEEN '01-jun-2023' AND SYSDATE
+    AND c.DATA BETWEEN '01-jul-2023' AND SYSDATE
     AND c.vlbonific = 0
     AND d.codsupervisor = a.codsupervisor
   GROUP BY a.codsupervisor, s.nome, d.codsupervisor, ad.adddida, ad.addjr,ad.gap
