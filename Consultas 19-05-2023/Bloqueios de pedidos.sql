bff395a628847d0177d997ceeec3d2f0d0b466fc
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-07-10 10:57:49 -0300

    Update Bloqueios de pedidos.sql

--- Consultas 19-05-2023/Bloqueios de pedidos.sql
+++ Consultas 19-05-2023/Bloqueios de pedidos.sql
@@ -1,21 +1,29 @@
-SELECT DISTINCT(a.NUMPED)"Nº PEDIDO",u.codsupervisor "Superv.",(u.codusur || ' - ' || SUBSTR(u.nome,4,9)) AS "Vendedor",
-                TO_CHAR(a.data, 'DD/MM') || ' às ' || b.hora||'H' Envio,
-                (CASE WHEN a.posicao like 'B' Then a.posicao||'loqueado' WHEN a.posicao like 'C' Then a.posicao||'ancelado' ELSE a.posicao||'iberado' END) Status,
-                'R$'||REPLACE(b.vltotal, '.', ',') "Valor R$",'|' "|",a.codcli|| ' - ' ||SUBSTR(d.cliente,0,30) Cliente,
-                (CASE WHEN b.obs IS NULL Then '|NAO ANALISADO|' ELSE b.obs END) "MOTIVO ANALISADO",
-                (SELECT LISTAGG(c.motivo, ' | ') WITHIN GROUP (ORDER BY c.motivo desc)FROM PCBLOQUEIOSPEDIDO c WHERE a.numped = c.numped) "MOTIVO SISTEMA"
-                
+SELECT DISTINCT(a.NUMPED)"Nº PEDIDO",
+       u.codsupervisor "Superv.",
+       (u.codusur || ' - ' || SUBSTR(u.nome,4,9)) AS Vendedor,
+       TO_CHAR(a.data, 'DD/MM') || ' às ' || b.hora||'H' AS Envio,
+       
+       (CASE WHEN a.posicao like 'B' Then a.posicao||'loqueado' 
+             WHEN a.posicao like 'C' Then a.posicao||'ancelado' 
+             ELSE a.posicao||'iberado' END) AS Status,
+             
+       'R$'||REPLACE(b.vltotal, '.', ',') "Valor R$", '|' "|",
+       a.codcli|| ' - ' ||SUBSTR(d.cliente,0,30) AS Cliente,
+       
+       (CASE WHEN b.obs IS NULL Then '|NAO ANALISADO|' 
+             ELSE b.obs END) AS "MOTIVO ANALISADO",
+             
+       (SELECT LISTAGG(c.motivo, ' | ') WITHIN GROUP (ORDER BY c.motivo desc)
+               FROM PCBLOQUEIOSPEDIDO c 
+               WHERE a.numped = c.numped) AS "MOTIVO SISTEMA"
+               
 FROM pcpedi a
     JOIN pcpedc b on a.numped = b.numped
     JOIN PCBLOQUEIOSPEDIDO c on a.numped = c.numped
     JOIN pcclient d on d.codcli = a.codcli 
     JOIN PCUSUARI U on U.codusur = a.codusur
-    
-WHERE a.data BETWEEN SYSDATE-5 and SYSDATE
-    AND a.codusur in 
-    (--140, 141, 142, 157,
-    143, 145, 148, 150, 151, 152, 153, 155, 156,  158, 161, 164, 167, 168, 169, 170, 172, 174, 2, 160)
+WHERE a.data BETWEEN SYSDATE-14 and SYSDATE
+    AND a.codusur in (140, 141, 142, 157,143, 145, 148, 150, 151, 152, 153, 155, 
+                      156,  158, 161, 164, 167, 168, 169, 170, 172, 174, 2, 160)
     AND a.posicao Not Like 'F' AND a.posicao Not Like 'M' 
-
-    
 ORDER BY "Nº PEDIDO"
