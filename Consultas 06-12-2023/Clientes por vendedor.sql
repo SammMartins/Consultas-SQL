f3b27d0c3ffc7dbb590199ed2bde2460ff664517
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-07-10 10:57:46 -0300

    Update Clientes por vendedor.sql

--- Consultas 06-12-2023/Clientes por vendedor.sql
+++ Consultas 06-12-2023/Clientes por vendedor.sql
@@ -1,12 +1,11 @@
 select a.codcli,
        a.codcli || ' - ' || a.cliente cliente
             
-           
 from PCCLIENT a
     join pcusuari c on a.codusur1 = c.codusur
 
 where c.codusur in (140,141,142,145,148,149,150,151,152,153,155,
-                   156,157,158,160,164,167,168,169,170,172,174) --({VENDEDOR})
+                    156,157,158,160,164,167,168,169,170,172,174)
 
 group by a.codcli, a.cliente
 
