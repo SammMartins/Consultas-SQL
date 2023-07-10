8cc2f07dc9c94720d927888c8067f77da193b5f1
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-07-10 10:57:52 -0300

    Update Campanha Gulositos.sql

--- Consultas 28-06-2023/Campanha Gulositos.sql
+++ Consultas 28-06-2023/Campanha Gulositos.sql
@@ -1,3 +1,4 @@
+----------------------CTE PARA CONSULTAR O FATURAMENTO---------------------------
 WITH Fat AS 
     (SELECT ped.codusur,
             SUM(ped.qt*ped.pvenda) + 0 as Fat
@@ -8,7 +9,7 @@ WITH Fat AS
         AND ped.posicao NOT LIKE 'C'
         AND ped.vlbonific = 0
     Group By ped.codusur),
------------------------------------------------------------------------------------------------------------------------
+----------------------CTE PARA CONSULTAR A POSITIVAÇÃO--------------------------- 
     DN AS
         (SELECT ped.codusur,
                 COUNT(DISTINCT ped.codcli) + 0 AS DN
@@ -20,15 +21,12 @@ WITH Fat AS
                 AND PED.DTCANCEL IS NULL
                 AND PED.CONDVENDA IN (1, 2, 3, 7, 9, 14, 15, 17, 18, 19, 98)
             GROUP BY ped.codusur)
------------------------------------------------------------------------------------------------------------------------
-             
+-----------------------------CONSULTA PRINCIPAL----------------------------------
 SELECT  usur.codusur cod, usur.nome AS "Vendedor",
         f.fat AS "Faturamento",
         d.dn AS "DN DISTINTOS"
-        
 FROM PCUSUARI usur
         JOIN Fat f on usur.codusur = f.codusur
         JOIN DN d on usur.codusur = d.codusur
-
-WHERE usur.codusur in (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 156, 157, 158, 161, 164, 167, 168, 169, 170, 172, 174)
-
+WHERE usur.codusur in (140, 141, 142, 143, 145, 148, 150, 151, 152, 153, 155, 
+                        156, 157, 158, 161, 164, 167, 168, 169, 170, 172, 174) 
